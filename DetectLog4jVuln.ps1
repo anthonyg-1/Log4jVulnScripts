ac8fe7c57d7962d2e2c383ce5f42bffc3c94577b
Tony Guimelli <tony.guimelli@gmail.com>
2021-12-14 16:47:24 -0500

    Update DetectLog4jVuln.ps1
    Added comment-based help.

--- DetectLog4jVuln.ps1
+++ DetectLog4jVuln.ps1
@@ -1,3 +1,21 @@
+<#
+    .SYNOPSIS
+        DetectLog4jVuln.ps1
+    .DESCRIPTION
+        This PowerShell script serves to detect CVE-2021-44228 (Log4j vulnerability) on Windows systems.
+    .EXAMPLE
+        .\DetectLog4jVuln.ps1
+
+        Searches all file system drives for solr.in.cmd and determines if mitigation needs to be applied.
+    .NOTES
+        Requires administrative privileges and PowerShell version 4.0 or higher.
+    .LINK
+        https://solr.apache.org/security.html#apache-solr-affected-by-apache-log4j-cve-2021-44228
+        https://blog.malwarebytes.com/exploits-and-vulnerabilities/2021/12/log4j-zero-day-log4shell-arrives-just-in-time-to-ruin-your-weekend/
+        https://github.com/anthonyg-1/Log4jVulnScripts
+
+#>
+
 #requires -RunAsAdministrator
 #requires -Version 4
 
@@ -36,7 +54,7 @@ foreach ($driveLetter in $drives) {
 
                 if ($fileContent -match $mitigation) {
                     $fileIsPatched = $true
-                    Write-Verbose "$filePath is already patched." -Verbose
+                    Write-Verbose "Mitigation already applied on $filePath." -Verbose
                 }
                 else {
                     Write-Warning "Mitigation not currently applied on $filePath."
