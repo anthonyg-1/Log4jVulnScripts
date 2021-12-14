6adaa0f31b41ad18e5b2e278851d88177e4fb137
Tony Guimelli <tony.guimelli@gmail.com>
2021-12-14 10:26:42 -0500

    Add files via upload
    Initial commit

---
+++ MitigateLog4jVuln.ps1
@@ -0,0 +1,137 @@
+#requires -RunAsAdministrator
+#requires -Version 4
+
+#region Script Parameters:
+
+Param
+(
+    # If no drive is specified, all are searched:
+    [Parameter(Mandatory = $false, Position = 1)][ValidateSet("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z")][String]$DriveLetter = "D",
+
+    # Determines if we are going to restart the SOLR service when executed. Default value is false:
+    [Parameter(Mandatory = $false, Position = 0)][Switch]$RestartService
+)
+
+#endregion
+
+
+#region Configuration Values
+
+# Determines if we are going to restart the SOLR service when executed. Default value is false:
+[bool]$RestartService = $false
+
+# The file that we will attempt to write to:
+[string]$targetFileName = "solr.in.cmd"
+
+# Entry to add to target file:
+[string]$mitigation = "set SOLR_OPTS=%SOLR_OPTS% -Dlog4j2.formatMsgNoLookups=true"
+
+# Minimum version that this mitigation requires:
+$targetFileVersionMinimum = 2.15
+
+#endregion
+
+
+#region Functions
+
+# Retrieve SOLR service name and version:
+function Get-SolrServiceDetail {
+    $solServiceSearchString = "sol"
+    $targetService = Get-WmiObject Win32_Service | Where-Object Name -Match $solServiceSearchString
+
+    if ($null -eq $targetService) {
+        $FileNotFoundException = New-Object -TypeName IO.FileNotFoundException -ArgumentList "Sitecore Search Service not found."
+        Write-Error -Exception $FileNotFoundException -ErrorAction Stop
+    }
+
+    $exePath = $targetService.PathName -replace '"', ""
+    $solrVersion = (Get-Item -Path $exePath).VersionInfo.FileVersion
+    $solServiceName = $targetService.Name
+
+    return $([PSCustomObject]@{ServiceName = $solServiceName; ServiceVersion = $solrVersion })
+}
+
+#endregion
+
+
+#region Checks
+
+# If minimum version is not met, throw a terminating exception:
+if ((Get-SolrServiceDetail).ServiceVersion -le $targetFileVersionMinimum) {
+    $argumentExceptionMessage = "This script is only applicable to SOLR versions $targetFileVersionMinimum or higher. Execution halted."
+    $ArgumentException = New-Object -TypeName ArgumentException -ArgumentList $argumentExceptionMessage
+    Write-Error -Exception $ArgumentException -ErrorAction Stop
+}
+
+#endregion
+
+
+#region Main
+
+# If a drive is specified use that else search all:
+$drives = @()
+if ($PSBoundParameters.ContainsKey("DriveLetter")) {
+    $drives += $DriveLetter
+}
+else {
+    $drives = Get-PSDrive | Where-Object { $_.Provider.Name -like "*File*" } | Select-Object -ExpandProperty Name
+}
+
+# Get the service name:
+$serviceName = (Get-SolrServiceDetail).ServiceName
+
+# Clear console between each run:
+Clear-Host
+
+# Iterate through each drive on the system, find the target files, and apply fix. Optionally restart the service as specified in the $RestartService variable at the top of this script:
+foreach ($driveLetter in $drives) {
+    $targetDrive = $driveLetter + ":"
+
+    Write-Verbose "Searching for $targetFileName in $driveLetter drive..." -Verbose
+
+    $targetFiles = Get-Childitem -Path $targetDrive -Include $targetFileName -Recurse -File -ErrorAction SilentlyContinue
+
+    if ($targetFiles.Count -gt 0) {
+        Write-Verbose "Found $targetFileName. Determining if mitigation needs to be applied..." -Verbose
+        foreach ($foundFile in $targetFiles) {
+            [bool]$fileIsPatched = $false
+
+            try {
+                $filePath = $foundFile.FullName
+
+                $fileContent = Get-Content -Path $filePath -Raw
+
+                if ($fileContent -match $mitigation) {
+                    $fileIsPatched = $true
+                    Write-Verbose "$filePath is already patched. No action taken." -Verbose
+                }
+                else {
+                    Write-Warning "Mitigation not currently applied. Applying mitigation to the following file: $filePath"
+
+                    # Add a carriage return, REM line (comment), and the mitigation:
+                    Add-Content -Path $filePath -Value "`r`n"
+                    Add-Content -Path $filePath -Value "REM log4j vulnerability mitigation:"
+                    Add-Content -Path $filePath -Value $mitigation -Verbose
+
+                    if ($PSBoundParameters.ContainsKey("RestartService")) {
+                        if (-not($fileIsPatched)) {
+                            Write-Verbose "Restarting the $serviceName service" -Verbose
+                            Restart-Service -Name $serviceName -Force -Verbose
+                        }
+                    }
+                    else {
+                        Write-Warning "Please restart the following service for the mitigation to take effect: $serviceName"
+                    }
+                }
+            }
+            catch {
+                Write-Error -Exception $_.Exception -ErrorAction Continue
+            }
+        }
+    }
+    else {
+        Write-Warning "$targetFileName not found in $targetDrive"
+    }
+}
+
+#endregion
